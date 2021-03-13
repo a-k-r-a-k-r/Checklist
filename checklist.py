7c1daee16d17e837cb2caa07b300f0501c89f704
a-k-r-a-k-r <akr_akr@protonmail.com>
2021-03-13 13:17:28 +0530

    Resolved minor bugs

--- checklist.py
+++ checklist.py
@@ -1,7 +1,27 @@
+'''
+    Program: Checklist
+    Author : akr
+    GitHub : https://github.com/a-k-r-a-k-r
+    Blog   : https://a-k-r-a-k-r.blogspot.com
+'''
+
+
 #Simple Checklist
 import tkinter
-from tkinter import END, ANCHOR
+from tkinter import END, ANCHOR, filedialog
 count=0
+
+
+#Define fonts and colors
+my_font = ('Times New Roman', 12)
+root_color = 'black'
+list_entry_bg = "lightblue"
+list_entry_fg = "blue"
+listbox_bg = "black"
+listbox_fg = "red"
+button_color = 'lightblue'
+
+
 
 #Define window
 root = tkinter.Tk()
@@ -9,16 +29,11 @@ root.title('My Checklist')
 root.iconbitmap('resources/icons/checklist.ico')
 root.geometry('400x400')
 root.resizable(0,0)
-
-#Define fonts and colors
-my_font = ('Times New Roman', 12)
-root_color = '#6c1cbc'
-button_color = '#e2cff4'
 root.config(bg=root_color)
+
 
 #Define functions
 def add_item():
-    """Add an individual item to the listbox"""
     global count
     count=count+1
     my_listbox.insert(END,str(count) + ") " + list_entry.get())
@@ -26,37 +41,49 @@ def add_item():
 
 
 def remove_item():
-    """Remove the selected (ANCHOR) item from the listbox"""
     my_listbox.delete(ANCHOR)
 
+
 def clear_list():
-    """Delete all items from the listbox"""
     global count
     my_listbox.delete(0, END)
     count=0
 
 
 def save_list():
-    """Save the list to a simple txt file"""
-    with open('checklist.txt', 'w') as f:
-        #listbox.get() returns a tuple....
+    global path
+    path=filedialog.asksaveasfilename(initialdir="./saved_lists",title="Save List",filetype=(("Text File","*.txt"),("All Files","*.*")))
+    path=path+".txt"
+    with open("last_file_path.txt","w") as last_file:
+        last_file.write(path)
+    with open(path, 'w') as f:
         list_tuple = my_listbox.get(0, END)
         for item in list_tuple:
-            #Take proper precautions to include only one \n for formatting purposes
             if item.endswith('\n'):
                 f.write(item)
             else:
                 f.write(item + "\n")
 
 
-def open_list():
-    """Open the list upon starting the program if there is one"""
+def open_last_list():
     try:
-        with open('checklist.txt', 'r') as f:
+        with open('last_file_path.txt', 'r') as f:
+            path=f.read()
+        with open(path,"r") as f:
             for line in f:
                 my_listbox.insert(END, line)
     except:
         return
+
+def open_saved():
+    clear_list()
+    path=filedialog.askopenfile(initialdir="./saved_lists",title="Checklists",filetype=(("Text File","*.txt"),("All Files","*.*")))
+    path=path.name
+    with open("last_file_path.txt","w") as last_file:
+        last_file.write(path)
+    with open(path,"r") as f:
+        for line in f:
+            my_listbox.insert(END, line)
 
 
 #Define layout
@@ -69,14 +96,14 @@ output_frame.pack()
 button_frame.pack()
 
 #Input frame layout
-list_entry = tkinter.Entry(input_frame, width=35, borderwidth=3, font=my_font)
+list_entry = tkinter.Entry(input_frame, width=35, borderwidth=3, font=my_font,bg=list_entry_bg,fg=list_entry_fg)
 list_add_button = tkinter.Button(input_frame, text="Add Item", borderwidth=2,font=my_font, bg=button_color, command=add_item)
 list_entry.grid(row=0, column=0, padx=5, pady=5)
 list_add_button.grid(row=0, column=1, padx=5, pady=5, ipadx=5)
 
 #Output frame layout
 my_scrollbar = tkinter.Scrollbar(output_frame)
-my_listbox = tkinter.Listbox(output_frame, height=15, width=45, borderwidth=3,font=my_font, yscrollcommand=my_scrollbar.set)
+my_listbox = tkinter.Listbox(output_frame, height=15, width=45, borderwidth=3,font=my_font,bg=listbox_bg,fg=listbox_fg ,yscrollcommand=my_scrollbar.set)
 #Link scrollbar to listbox
 my_scrollbar.config(command=my_listbox.yview)
 my_listbox.grid(row=0, column=0)
@@ -86,14 +113,15 @@ my_scrollbar.grid(row=0, column=1, sticky="NS")
 list_remove_button = tkinter.Button(button_frame, text="Remove Item", borderwidth=2,font=my_font, bg=button_color, command=remove_item)
 list_clear_button = tkinter.Button(button_frame, text='Clear List', borderwidth=2,font=my_font, bg=button_color, command=clear_list)
 save_button = tkinter.Button(button_frame, text='Save List', borderwidth=2,font=my_font, bg=button_color, command=save_list)
-quit_button = tkinter.Button(button_frame, text='Quit', borderwidth=2, font=my_font,bg=button_color, command=root.destroy)
+quit_button = tkinter.Button(button_frame, text='Open', borderwidth=2, font=my_font,bg=button_color, command=open_saved)
 list_remove_button.grid(row=0, column=0, padx=2, pady=10)
 list_clear_button.grid(row=0, column=1, padx=2, pady=10, ipadx=10)
 save_button.grid(row=0, column=2, padx=2, pady=10, ipadx=10)
 quit_button.grid(row=0, column=3, padx=2, pady=10, ipadx=25)
 
+
 #Open the previous list if available
-open_list()
+open_last_list()
 
 #Run the root window's main loop
 root.mainloop()
